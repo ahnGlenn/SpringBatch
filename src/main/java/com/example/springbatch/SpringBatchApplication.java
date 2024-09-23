cb59d473dffa1ce585590005316818037fcf9fe2
ahnGlenn <dkstkdwo93@naver.com>
2024-09-23 11:44:25 +0900

    h2 DB connect

--- src/main/java/com/example/springbatch/SpringBatchApplication.java
+++ src/main/java/com/example/springbatch/SpringBatchApplication.java
@@ -1,8 +1,16 @@
 package com.example.springbatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * 스프링부트 실행 클래스
+ * @EnableBatchProcessing : Spring Batch 기능 활성화
+ * @SpringBootApplication : Spring Application을 설정
+ */
+@EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchApplication {
 
