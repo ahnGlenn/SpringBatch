435faa06307043d7c9d059ff134988a6b53bc11d
ahnGlenn <dkstkdwo93@naver.com>
2024-10-02 11:12:58 +0900

    fix : to run Spring Batch when Application starts.

--- src/main/java/com/example/springbatch/SpringBatchApplication.java
+++ src/main/java/com/example/springbatch/SpringBatchApplication.java
@@ -1,8 +1,15 @@
 package com.example.springbatch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -14,8 +21,24 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class SpringBatchApplication {
 
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job importUserJob;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBatchApplication.class, args);
     }
 
+    // 애플리케이션 실행 후 배치 작업 시작
+    @Bean
+    public CommandLineRunner run() {
+        return args -> {
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addLong("time", System.currentTimeMillis()) // 유니크한 JobParameters 생성
+                    .toJobParameters();
+            jobLauncher.run(importUserJob, jobParameters);
+        };
+    }
 }
