7487992890c2c0fdb1c9d5b29aaeb39050b962a2
ahnGlenn <dkstkdwo93@naver.com>
2024-10-02 15:45:23 +0900

    dev : h2 - batch connect (fail_2)

--- src/main/java/com/example/springbatch/SpringBatchApplication.java
+++ src/main/java/com/example/springbatch/SpringBatchApplication.java
@@ -1,5 +1,6 @@
 package com.example.springbatch;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
@@ -17,7 +18,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * // @EnableBatchProcessing : Spring Batch 기능 활성화
  * // @SpringBootApplication : Spring Application을 설정
  */
-// @EnableBatchProcessing
+@EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchApplication {
 
@@ -32,21 +33,19 @@ public class SpringBatchApplication {
     }
 
     // 애플리케이션 실행 후 배치 작업 시작
-    @Bean
-    public CommandLineRunner run() {
-        return args -> {
-            // 이 부분에서 잠시 대기하여 DB가 초기화된 후 실행
-            Thread.sleep(5000); // 5초 대기 (이 시간은 적절히 조정 가능)
-
-            JobParameters jobParameters = new JobParametersBuilder()
-                    .addLong("time", System.currentTimeMillis())
-                    .toJobParameters();
-
-            try {
-                jobLauncher.run(importUserJob, jobParameters);
-            } catch (Exception e) {
-                e.printStackTrace(); // 오류 로깅
-            }
-        };
-    }
+//    @Override
+//    public void run(String... args) throws Exception {
+//        JobParameters jobParameters = new JobParametersBuilder()
+//                .addLong("time", System.currentTimeMillis())
+//                .toJobParameters();
+//
+//        // 비동기 작업 실행
+//        new Thread(() -> {
+//            try {
+//                jobLauncher.run(importUserJob, jobParameters);
+//            } catch (Exception e) {
+//                e.printStackTrace(); // 오류 로깅
+//            }
+//        }).start();
+//    }
 }
